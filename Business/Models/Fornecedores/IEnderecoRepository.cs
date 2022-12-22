8c0bbc45e0db29a8040dd30491176b1cbfdc22b1
Breno Gomes <breno.jgomes2@gmail.com>
2022-12-22 01:02:03 -0300

    add IRepository

---
+++ Business/Models/Fornecedores/IEnderecoRepository.cs
@@ -0,0 +1,11 @@
+﻿using Business.Core.Data;
+using System;
+using System.Threading.Tasks;
+
+namespace Business.Models.Fornecedores
+{
+    interface IEnderecoRepository : IRepository<Endereco>
+    {
+        Task<Endereco> ObterEnderecoPorFornecedor(Guid fornecedorId);
+    }
+}
---
+++ Business/Models/Produtos/IProdutoRepository.cs
@@ -0,0 +1,14 @@
+﻿using Business.Core.Data;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Business.Models.Produtos
+{
+    interface IProdutoRepository : IRepository<Produto>
+    {
+        Task<IEnumerable<Produto>> ObeterProdutosPorFornecedor(Guid fornecedorId); 
+        Task<IEnumerable<Produto>> ObeterProdutosFornecedores(); 
+        Task<Produto> ObeterProdutoFornecedor(Guid id); 
+    }
+}
