d15acdf9a91f9bb635152e27837118597a323858
Breno Gomes <breno.jgomes2@gmail.com>
2023-01-04 00:11:08 -0300

    Services

---
+++ Business/Core/Services/BaseService.cs
@@ -0,0 +1,14 @@
+﻿using Business.Core.Models;
+using FluentValidation;
+
+namespace Business.Core.Services
+{
+    public abstract class BaseService
+    {
+        protected bool ExecutarValidacao<TV, TE>(TV validacao, TE entidade) where TV : AbstractValidator<TE> where TE : Entity
+        {
+            var validator = validacao.Validate(entidade);
+            return validator.IsValid;
+        }
+    }
+}
---
+++ src/AppMvc/Controllers/FornecedoresController.cs
@@ -0,0 +1,17 @@
+﻿using Business.Models.Fornecedores;
+using Business.Models.Fornecedores.Services;
+using Infra.Data.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppMvc.Controllers
+{
+    public class FornecedoresController : Controller
+    {
+       
+    }
+}
